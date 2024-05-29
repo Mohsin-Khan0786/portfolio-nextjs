2c0684d625d09514a0c1805cbfe6a977c9091fe1
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2024-05-29 09:58:18 +0500

    set home & nav

--- src/components/ui/Home.jsx
+++ src/components/ui/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <>
       <div className="main">
         <div className="title">
-          <h5>Hello 👋, 1 an </h5>
+          <h5>Hello 👋, I an </h5>
           <h1>Mohsin Khan</h1>
           <h3>A Passionate Web Developer</h3>
           <p>
