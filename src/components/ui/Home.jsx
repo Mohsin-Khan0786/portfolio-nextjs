343ee588c681bfee4e47b9e321fd31ef8849b95f
Mohsin Khan <ranamohsin4520@gmail.com>
2024-05-30 22:55:09 +0500

    change

--- src/components/ui/Home.jsx
+++ src/components/ui/Home.jsx
@@ -3,41 +3,38 @@
 import React from "react";
 import Image from "next/image";
 import "@/components/style/home.css";
+
 const Home = () => {
   return (
-    <>
-      <div className="main">
-        <div className="title">
-          <h5>Hello 👋, I am</h5>
-          <h1>Mohsin Khan</h1>
-          <h3>A Passionate Web Developer</h3>
-          <p>
-            I'm a Full-Stack Developer with 6 months of hands-on experience in
-            designing, developing, and implementing applications and solutions
-            using a range of technologies and programming languages.
-          </p>
-          <div className="social">
-            <a href="./mohsin khAN cv pdf.pdf" download>
-            <button >My Resume</button>
-            </a>
-           
-
-       <a href="https://github.com/Mohsin-Khan0786" target="_blank" rel="noopener noreferrer">
-      <button>My GitHub</button>
-    </a>
-          </div>
-        </div>
-
-        <div className="profile">
-          <Image
-            src={"/mohsin.png.png"}
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
+    <div className="main">
+      <div className="title">
+        <h5>Hello 👋, I am</h5>
+        <h1>Mohsin Khan</h1>
+        <h3>A Passionate Web Developer</h3>
+        <p>
+          I'm a Full-Stack Developer with 6 months of hands-on experience in
+          designing, developing, and implementing applications and solutions
+          using a range of technologies and programming languages.
+        </p>
+        <div className="social">
+          <a href="/mohsin khAN cv pdf.pdf" download>
+            <button>My Resume</button>
+          </a>
+          <a href="https://github.com/Mohsin-Khan0786" target="_blank" rel="noopener noreferrer">
+            <button>My GitHub</button>
+          </a>
         </div>
       </div>
-    </>
+
+      <div className="profile">
+        <Image n  
+          src="/mohsin.png.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </div>
+    </div>
   );
 };
 
